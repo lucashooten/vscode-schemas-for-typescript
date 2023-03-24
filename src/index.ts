b962a4372aedab0722343d3e787e3e8125ed0ba4
Lucas Hooten <lucashooten@gmail.com>
2023-03-23 23:34:10 -0700

    pullups give strong arms

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,41 @@
 import gulp, {src, dest} from 'gulp'
 import replace from 'gulp-replace'
-import { compileFromFile, compile } from 'json-schema-to-typescript'
-import { repository } from '../package.json'
+import { compileFromFile } from 'json-schema-to-typescript'
 import fs from 'fs'
 
-const repoUrl = repository.replace('.git','').concat('/')
-
-
-export const replaceVSCodeHandles = () => {
+export const generateSchemasAndTypes = async () => {
     const vscodeUriAddress = /vscode:\/\/([^"]+)/g
 
-    return src('./src/wraith13-vscode-schemas/en/latest/schemas/**/*') // @ts-ignore
-        //.pipe(debug({title: 'found file: '}))
-        .pipe(replace(vscodeUriAddress, `https://raw.githubusercontent.com/lucashooten/vscode-schemas-for-typescript/main/$1`+'.json'))
-        //.pipe(debug({title: 'replacer: '}))
-        .pipe(dest('./schemas/'))
-};
+    const replaceVSCodeHandles = () => {
+        return new Promise((res, rej) => {
+            src('./src/wraith13-vscode-schemas/en/latest/schemas/**/*')
+                .pipe(replace(vscodeUriAddress, `https://raw.githubusercontent.com/lucashooten/vscode-schemas-for-typescript/main/$1` + '.json'))
+                .pipe(dest('./schemas/'))
+                .on('error', rej)
+                .on('end', res)
+        })
+    };
 
-export const generateTypes = async () => {
-    const schemas = fs.readdirSync('./schemas/')
-    console.log(schemas)
-    for (const schemaFileName of schemas) {
-        const schemaFile = fs.readFileSync('./schemas/'+schemaFileName).toString('utf8')
-        const typeFromSchema = await compileFromFile('./schemas/'+schemaFileName);
-        fs.writeFileSync(`./types/${schemaFileName.replace('.json', '.ts')}`, typeFromSchema);
-    }
+    const generateTypes = async () => {
+        console.log('Generating types...')
+        const schemas = [
+            ...fs.readdirSync('./schemas'),
+            ...fs.readdirSync('./schemas/settings').map(i => 'settings/'+i)
+        ]
+        console.log({schemas})
+        for (const schemaFileName of schemas) {
+            try {
+                const typeFromSchema = await compileFromFile('./schemas/' + schemaFileName);
+                fs.writeFileSync(`./types/${schemaFileName.replace('.json', '.ts')}`, typeFromSchema);
+            } catch (e) {
+                console.log('error parsing: '+schemaFileName, e)
+            }
+        }
+    };
+
+    return replaceVSCodeHandles()
+        .then(() => generateTypes())
+        .catch((e) => console.log(e))
 }
 
-replaceVSCodeHandles()
-setTimeout(() => generateTypes(), 5000)
+generateSchemasAndTypes().then(() => console.log('hi'))
